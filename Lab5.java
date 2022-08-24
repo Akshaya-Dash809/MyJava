a950540f6adb8144e1d471300a0f21300f53499b
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-08-24 14:22:27 +0530

    Add files via upload

---
+++ Lab5.java
@@ -0,0 +1,67 @@
+package com.Lab;
+import java.util.*;
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		/* 1. Write a program in Java to display the pattern like right angle triangle with a number
+
+		for(int i=1;i<=10;i++)
+		{
+		int num=1;
+		for(int j=1;j<i+1;j++)
+		{
+			System.out.print(num+"");
+			num=num+1;
+		}
+		System.out.println();
+		}     */                         
+			
+		/* 2. Write a program in Java to make such a pattern like right angle triangle with number increased by 1.The pattern like : 
+
+            1
+            2 3
+            4 5 6
+            7 8 9 10
+		
+		int i,j,k=1;
+		for(i=1;i<=4;i++) {
+			for(j=1;j<i+1;j++) {
+				System.out.print(k++ +" ");
+			}
+			System.out.println();
+		}     */
+		
+		/* 3. Write a Java program to multiply two given integers without using the multiply operator(*).
+		
+		Scanner sc = new Scanner(System.in);
+		int x,y,sum=0;
+		System.out.println("Enter the first number  ");
+		x=sc.nextInt();
+		System.out.println("Enter the second number  ");
+		y = sc.nextInt();
+		
+		for(int i=1;i<=x;i++) {
+			sum=sum+y;
+		}
+        	System.out.println("The multiplication of "+x+" and y is "+sum);   */
+		
+		  /* 4. Write a Java program that takes the user to provide a single character from the alphabet. Print Vowel or Consonant, depending on the user input. If the user input is not a letter (between a and z or A and Z),
+		   
+		    char ch = 'b';
+        	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' ||ch == 'E' || ch == 'I' || ch == 'O' || ch == 'u' ) { 
+        	System.out.println("This is a vowel ");
+        	}
+        	
+        	else {
+        		System.out.println("This is a consonant");
+        	}     */
+        	
+        	
+        }
+		
+	}
+
+
