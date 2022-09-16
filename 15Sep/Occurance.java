9ba6d807c99649acea859de7a732f14a48b81001
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-09-16 21:16:13 +0530

    Add files via upload

---
+++ 15Sep/Occurance.java
@@ -0,0 +1,32 @@
+package lab_15Sep;
+import java.util.*;
+
+public class Occurance {
+		public static void main(String args[]) {
+
+					
+				String s="Hello,world";
+				int count1=0,count2=0;
+				for(int i=0;i<s.length();i++)
+				{
+					if(s.charAt(i)=='o') {
+						count1=i;
+						break;
+					}
+				}
+				
+				for(int i=0;i<s.length();i++)
+				{
+					if(s.charAt(i)=='o') {
+						count2=i;
+					}
+				}
+				System.out.println("First occurance of 'o' is : "+count1+" , "+"Second occurance of 'o' is "+count2);
+				
+		}
+			
+		}
+					
+	
+
+
