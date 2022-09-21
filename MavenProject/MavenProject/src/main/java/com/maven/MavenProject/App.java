9f24de92995e6a684e509475ee2c7e10e09c867e
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-09-21 16:59:21 +0530

    Add files via upload

---
+++ MavenProject/MavenProject/src/main/java/com/maven/MavenProject/App.java
@@ -0,0 +1,11 @@
+package com.maven.MavenProject;
+
+/**
+ * Print Message!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("I am Akshaya Kumar Dash");
+	}
+}
